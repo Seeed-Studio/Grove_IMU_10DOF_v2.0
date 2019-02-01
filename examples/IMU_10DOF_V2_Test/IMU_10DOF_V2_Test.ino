b0756d3e74f1b9024771197911077fef53bba828
Priveyes <3041616+Pascal66@users.noreply.github.com>
2019-02-01 10:56:33 +0100

    Update IMU_10DOF_V2_Test.ino
    Wire to 400Mhz

--- examples/IMU_10DOF_V2_Test/IMU_10DOF_V2_Test.ino
+++ examples/IMU_10DOF_V2_Test/IMU_10DOF_V2_Test.ino
@@ -54,15 +54,15 @@ float atm;
 float altitude;
 BMP280 bmp280;
 
-void setup()
-{
+void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
-
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  
     // initialize serial communication
     // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
     // it's really up to you depending on your project)
-    Serial.begin(38400);
+    Serial.begin(115200);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
@@ -77,11 +77,9 @@ void setup()
     Serial.println("     ");
 
     //  Mxyz_init_calibrated ();
-
 }
 
-void loop()
-{
+void loop() {
 
     getAccel_Data();
     getGyro_Data();
@@ -96,8 +94,7 @@ void loop()
     Serial.print("         ");
     Serial.println(mz_centre);
     Serial.println("     ");
-
-
+    
     Serial.println("Acceleration(g) of X,Y,Z:");
     Serial.print(Axyz[0]);
     Serial.print(",");
@@ -145,18 +142,15 @@ void loop()
 
     Serial.println();
     delay(1000);
-
 }
 
 
-void getHeading(void)
-{
+void getHeading(void) {
     heading = 180 * atan2(Mxyz[1], Mxyz[0]) / PI;
     if (heading < 0) heading += 360;
 }
 
-void getTiltHeading(void)
-{
+void getTiltHeading(void) {
     float pitch = asin(-Axyz[0]);
     float roll = asin(Axyz[1] / cos(pitch));
 
@@ -167,11 +161,7 @@ void getTiltHeading(void)
     if (yh < 0)    tiltheading += 360;
 }
 
-
-
-void Mxyz_init_calibrated ()
-{
-
+void Mxyz_init_calibrated () {
     Serial.println(F("Before using 9DOF,we need to calibrate the compass frist,It will takes about 2 minutes."));
     Serial.print("  ");
     Serial.println(F("During  calibratting ,you should rotate and turn the 9DOF all the time within 2 minutes."));
@@ -195,9 +185,7 @@ void Mxyz_init_calibrated ()
     Serial.println("    ");
 }
 
-
-void get_calibration_Data ()
-{
+void get_calibration_Data () {
     for (int i = 0; i < sample_num_mdate; i++)
     {
         get_one_sample_date_mxyz();
@@ -208,8 +196,6 @@ void get_calibration_Data ()
         Serial.print(" ");
         Serial.println(mz_sample[2]);
         */
-
-
 
         if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
         if (my_sample[2] >= my_sample[1])my_sample[1] = my_sample[2]; //find max value
@@ -229,46 +215,34 @@ void get_calibration_Data ()
     my_min = my_sample[0];
     mz_min = mz_sample[0];
 
-
-
     mx_centre = (mx_max + mx_min) / 2;
     my_centre = (my_max + my_min) / 2;
     mz_centre = (mz_max + mz_min) / 2;
 
 }
 
-
-
-
-
-
-void get_one_sample_date_mxyz()
-{
+void get_one_sample_date_mxyz() {
     getCompass_Data();
     mx_sample[2] = Mxyz[0];
     my_sample[2] = Mxyz[1];
     mz_sample[2] = Mxyz[2];
 }
 
-
-void getAccel_Data(void)
-{
+void getAccel_Data(void) {
     accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
     Axyz[0] = (double) ax / 16384;
     Axyz[1] = (double) ay / 16384;
     Axyz[2] = (double) az / 16384;
 }
 
-void getGyro_Data(void)
-{
+void getGyro_Data(void) {
     accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
     Gxyz[0] = (double) gx * 250 / 32768;
     Gxyz[1] = (double) gy * 250 / 32768;
     Gxyz[2] = (double) gz * 250 / 32768;
 }
 
-void getCompass_Data(void)
-{
+void getCompass_Data(void) {
     I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
     delay(10);
     I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
@@ -282,8 +256,7 @@ void getCompass_Data(void)
     Mxyz[2] = (double) mz * 1200 / 4096;
 }
 
-void getCompassDate_calibrated ()
-{
+void getCompassDate_calibrated () {
     getCompass_Data();
     Mxyz[0] = Mxyz[0] - mx_centre;
     Mxyz[1] = Mxyz[1] - my_centre;
